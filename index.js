d2f398945e40e53816416676b39947321673d09b
Francesco Menzani <francesco@menzani.eu>
2020-07-20 22:57:25 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict"
+
+function selectText(node) {
+    if (document.body.createTextRange) {
+        const range = document.body.createTextRange()
+        range.moveToElementText(node)
+        range.select()
+    } else if (window.getSelection) {
+        const selection = window.getSelection()
+        const range = document.createRange()
+        range.selectNodeContents(node)
+        selection.removeAllRanges()
+        selection.addRange(range)
+    } else {
+        console.warn("Could not select text in node: Unsupported browser.")
+    }
+}
+
+function showCode(button) {
+	button.style.cursor = "auto"
+	const code = button.getElementsByTagName("pre")[0]
+	code.style.display = "block"
+}
